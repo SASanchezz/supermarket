7c543c9d95c50608c7cfe41a74ea5ff2f37bf4f8
Sanchez <sgrachov42@gmail.com>
2022-03-14 20:27:00 +0200

    added main window with list of users

---
+++ Data/DbMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermarket.Data
+{
+    class DbMaps
+    {
+        public enum EmployeeMap : int
+        {
+            id_employee,
+            empl_surname,
+            empl_name,
+            empl_patronumic,
+            empl_role,
+            salary,
+            date_of_birth,
+            date_of_start,
+            phone_number,
+            password,
+            city,
+            street,
+            zipcode
+        }
+    }
+}
