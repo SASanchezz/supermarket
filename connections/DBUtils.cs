fcf5e88502a48c4b6c7ae938eb443e31df0f3b0a
Sanchez <sgrachov42@gmail.com>
2022-03-14 20:31:43 +0200

    deleted unnecessary usings

--- connections/DBUtils.cs
+++ connections/DBUtils.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SqlClient;
+﻿using System.Collections.Generic;
 using System.Data.Common;
-using System.Data;
 using MySql.Data.MySqlClient;
 
 namespace supermarket.Connections
