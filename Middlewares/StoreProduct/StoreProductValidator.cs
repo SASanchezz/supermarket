44e127b8e60a620ab4fbd15514104c04201fa0a2
Sanchez <sgrachov42@gmail.com>
2022-04-08 22:27:16 +0300

    Implemented functional for Store Product

---
+++ Middlewares/StoreProduct/StoreProductValidator.cs
@@ -0,0 +1,87 @@
+﻿using supermarket.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using strPrdct = supermarket.Data.DbMaps.StoreProductMap;
+
+namespace supermarket.Middlewares.StoreProduct
+{
+    class StoreProductValidator
+    {
+        private const string _DEFAULT_UPC = "999999999909";
+
+        public static string ValidateNotProm(string UPC, string idProduct, string sellingPrice, string productNumber)
+        {
+            if (UPC.Any(x => !char.IsDigit(x)) || UPC.Length > 12)
+            {
+                return "Невалідний UPC";
+            }
+            if (DbQueries.GetStoreProductByID(UPC).Any())
+            {
+                return "Товар з таким UPC вже існує";
+            }
+
+            try
+            {
+                int.Parse(idProduct);
+            } catch
+            {
+                return "Невалідний id продукту";
+            }
+            if (!DbQueries.GetProductByID(idProduct).Any())
+            {
+                return "Такого товару не існує";
+            }
+            try
+            {
+                double.Parse(sellingPrice);
+            } catch
+            {
+                return "Введена некоректна ціна";
+            }
+
+            try
+            {
+                int.Parse(productNumber);
+            }
+            catch
+            {
+                return "Введена некоректна кількість товару";
+            }
+
+            return ""; //Alright
+        }
+
+        public static string ValidateProm(string UPCFather, string UPCProm)
+        {
+
+            if (UPCFather.Any(x => !char.IsDigit(x)) || UPCProm.Length > 12)
+            {
+                return "Невалідний UPC";
+            }
+
+            if (DbQueries.GetStoreProductByID(UPCProm).Any())
+            {
+                return "Акційний товар з таким UPC вже існує";
+            }
+
+            if (UPCFather == _DEFAULT_UPC) return ""; //Alright
+            //Don't check other conditions because it has no sense, father upc wasn't changed
+            if (!DbQueries.GetStoreProductByID(UPCFather).Any())
+            {
+                return "UPC неакційного товару не існує";
+            }
+            //If such UPCFather already has UPC_Prom
+            string[] storeProduct = DbQueries.GetStoreProductByID(UPCFather)[0];
+            if (UPCProm != _DEFAULT_UPC && storeProduct[(int)strPrdct.UPC_prom] != null)
+            {
+                return "Цей товар вже має акційний товар";
+            }
+
+            return ""; //Alright
+        }
+
+    }
+}
---
+++ Windows/ManagerMenu/StoreProduct/ManagePromStoreProductWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using supermarket.Middlewares.StoreProduct;
+using supermarket.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using strPrdct = supermarket.Data.DbMaps.StoreProductMap;
+
+namespace supermarket.Windows.ManagerMenu.StoreProduct
+{
+    public partial class ManagePromStoreProductWindow : Window
+    {
+        private const string _DEFAULT_UPC = "999999999909";
+        private string _promStoreProductId;
+        private string[] _fatherStoreProduct;
+        public ManagePromStoreProductWindow(string promStoreProductId)
+        {
+            _promStoreProductId = promStoreProductId;
+            _fatherStoreProduct = DbQueries.GetStoreProductByPromUpc(_promStoreProductId)[0]; //Find not prom product with such upc_prom
+        InitializeComponent();
+            FillBoxes();
+        }
+
+        /*
+        * This method fills boxes in window with information from database
+        */
+        private void FillBoxes()
+        {
+
+            upcPromBox.Text = _promStoreProductId;
+            upcParentBox.Text = _fatherStoreProduct[(int)strPrdct.UPC]; ;
+        }
+
+        /*
+        * This method updates information in database from text boxes
+        */
+        public void UpdateClick(object sender, RoutedEventArgs e)
+        {
+            MainStoreProductWindow owner = (MainStoreProductWindow)Owner;
+            //Renew buttons in MainProductWindow
+            owner.DeleteOldButtons();
+
+            string upcProm = (upcPromBox.Text != _promStoreProductId) ? upcPromBox.Text : _DEFAULT_UPC;
+            string upcParent = (upcParentBox.Text != _fatherStoreProduct[(int)strPrdct.UPC]) ? upcParentBox.Text : _DEFAULT_UPC;
+
+            string result = StoreProductValidator.ValidateProm(upcParent, upcProm);
+            if (result.Length != 0)
+            {
+                MessageBox.Show(result);
+                return;
+            }
+
+            upcProm = upcPromBox.Text;
+            upcParent = upcParentBox.Text;
+            string[] fatherStoreProduct = DbQueries.GetStoreProductByID(upcParent)[0];
+
+
+            // Set null on Prom_UPC in case of UPC change
+            if (upcParentBox.Text != _fatherStoreProduct[(int)strPrdct.UPC])
+            {
+                string sqlSetNull = string.Format("UPDATE Store_Product SET " +
+                "UPC_Prom=null " +
+                "WHERE UPC_Prom='{0}'", _promStoreProductId);
+                DbUtils.Execute(sqlSetNull);
+            }
+
+            // Update father product table
+            string sqlFather = string.Format("UPDATE Store_Product SET " +
+                "UPC_Prom='{1}' " +
+                "WHERE UPC='{0}'",
+                upcParent, upcProm);
+            DbUtils.Execute(sqlFather);
+
+            // Update promotion product table
+            string sqlProm = string.Format("UPDATE Store_Product SET " +
+                "UPC='{1}', id_product={2}, selling_price={3}, products_number={4} " +
+                "WHERE UPC='{0}'",
+                _promStoreProductId, upcProm, fatherStoreProduct[(int)strPrdct.id_product], double.Parse(fatherStoreProduct[(int)strPrdct.selling_price]) * 0.8,
+                fatherStoreProduct[(int)strPrdct.products_number]);
+            DbUtils.Execute(sqlProm);
+
+            owner.SetButtons();
+            owner.Show();
+            Close();
+        }
+        /*
+         * Delete production unit from database
+         */
+        private void DeleteClick(object sender, RoutedEventArgs e)
+        {
+            MainStoreProductWindow owner = (MainStoreProductWindow)Owner; //So we can renew buttons 
+            owner.DeleteOldButtons();
+            DbQueries.DeleteStoreProductByID(_promStoreProductId);
+
+            string sqlDeleteProm = string.Format("UPDATE Store_Product SET " +
+                "UPC_Prom=null " +
+                "WHERE UPC_Prom='{0}'", _promStoreProductId);
+            DbUtils.Execute(sqlDeleteProm);
+
+
+            owner.SetButtons();
+            owner.Show();
+            Close();
+        }
+
+        private void ReturnClick(object sender, RoutedEventArgs e)
+        {
+            Owner.UpdateLayout();
+            Owner.Show();
+            Close();
+        }
+    }
+}
