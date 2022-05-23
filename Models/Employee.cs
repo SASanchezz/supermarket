6dcf13d66b88c744d46e2f03fe2dd82532b9ee38
Sanchez <sgrachov42@gmail.com>
2022-05-23 18:31:47 +0300

    fix employee sql query

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -19,7 +19,7 @@ namespace supermarket.Models
             string sql = String.Format("INSERT INTO Employee " +
                 "(empl_surname, empl_name, empl_patronymic, empl_role_id, salary, " +
                 "date_of_birth, date_of_start, phone_number, password, city, street, zip_code) " +
-                "VALUES ('{0}', '{1}', '{2}', '{3}', {4}, " +
+                "VALUES ('{0}', '{1}', '{2}', {3}, {4}, " +
                 "'{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}')",
                 surname, name, patronymic, Roles.roleKeys[role], float.Parse(salary),
                 dateBirth.ToString(s_format), dateStart.ToString(s_format),
