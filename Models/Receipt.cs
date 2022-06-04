17cfb6b8270df413cbd3a7f4d95917b90c73c79e
Lizzza <lizazhyvitskaya@gmail.com>
2022-06-04 18:49:12 +0300

    added receipts 2.0

---
+++ Models/Receipt.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using supermarket.Utils;
+
+namespace supermarket.Models
+{
+    internal static class Receipt
+    {
+        public const int receipt_number = 0;
+        public const int id_employee = 1;
+        public const int card_number = 2;
+        public const int print_date = 3;
+        public const int sum_total = 4;
+        public const int vat = 5;
+        // added
+        public const int name_employee = 6;
+        public const int surname_employee = 7;
+        public const int patronymic_employee = 8;
+        
+        //private static DateTime _minPrintDate = DateTime.Today.AddYears(-3); to validator
+        
+        public static List<string[]> GetAllReceipts(DateTime minPrintDate, DateTime maxPrintDate)
+        {
+            const string dateFormat = "yyyy-MM-dd";
+            string minPrintDateString = minPrintDate.ToString(dateFormat);
+            string maxPrintDateString = maxPrintDate.ToString(dateFormat);
+            
+            string sql = "SELECT receipt_number, Receipt.id_employee, card_number, print_date, sum_total, " +
+                         "vat, empl_name, empl_surname, empl_patronymic " +
+                         "FROM Receipt LEFT JOIN Employee ON Receipt.id_employee=Employee.id_employee " +
+                         $"WHERE DATE(print_date) >= '{minPrintDateString}' AND DATE(print_date) <= '{maxPrintDateString}'";
+            
+            List<string[]> result = DbUtils.FindAll(sql);
+            
+            return result.Count > 0 ? result : null;
+        }
+        
+    }
+}
---
+++ ViewModels/ManagerMenu/Receipts/ReceiptsWindowVM.cs
@@ -0,0 +1,10 @@
+using supermarket.ViewModels.BaseClasses;
+using supermarket.Windows.ManagerMenu.Receipts;
+
+namespace supermarket.ViewModels.ManagerMenu.Receipts
+{
+    internal class ReceiptsWindowVM : WindowViewModel<ReceiptsWindow, ReceiptsVM>
+    {
+        
+    }
+}
