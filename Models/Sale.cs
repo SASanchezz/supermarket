6e807302ca3c16570cea3472b62178a993bf6f58
Lizzza <lizazhyvitskaya@gmail.com>
2022-06-15 15:50:47 +0300

    finished sales manager

--- Models/Sale.cs
+++ Models/Sale.cs
@@ -11,11 +11,10 @@ namespace supermarket.Models
         private const int product_number = 2;
         private const int selling_price = 3;
 
-        public static List<string[]> GetAllSales(string idProduct,  DateTime minPrintDate, DateTime maxPrintDate)
+        public static List<string[]> GetAllSales(string productId, DateTime minPrintDate, DateTime maxPrintDate)
         {
-            string dateFormat = "yyyy-MM-dd";
-            string minPrintDateString = minPrintDate.ToString(dateFormat);
-            string maxPrintDateString = maxPrintDate.ToString(dateFormat);
+            string minPrintDateString = minPrintDate.ToString(Constants.DateStringFormat);
+            string maxPrintDateString = maxPrintDate.ToString(Constants.DateStringFormat);
             
             string sql = "SELECT Sale.UPC, Product.id_product, Product.product_name, Sale.selling_price, " +
                          "Sale.product_number, Receipt.print_date " +
@@ -26,9 +25,9 @@ namespace supermarket.Models
                          $"WHERE DATE(Receipt.print_date) >= '{minPrintDateString}' " +
                          $"AND DATE(Receipt.print_date) <= '{maxPrintDateString}'";
 
-            if (idProduct != Constants.AllString)
+            if (productId != Constants.AllString)
             {
-                sql += $" AND Product.id_product LIKE '%{idProduct}%'";
+                sql += $" AND Product.id_product LIKE '%{productId}%'";
             }
 
             List<string[]> result = DbUtils.FindAll(sql);
@@ -48,6 +47,28 @@ namespace supermarket.Models
             List<string[]> result = DbUtils.FindAll(sql);
             
             return result.Count > 0 ? result : null;
+        }
+
+        public static string GetSumOfNumberOfProducts(string productId, DateTime minPrintDate, DateTime maxPrintDate)
+        {
+            string minPrintDateString = minPrintDate.ToString(Constants.DateStringFormat);
+            string maxPrintDateString = maxPrintDate.ToString(Constants.DateStringFormat);
+            
+            string sql = "SELECT SUM(Sale.product_number) " +
+                         "FROM Sale " +
+                         "LEFT JOIN Store_Product ON Store_Product.UPC = Sale.UPC " +
+                         "LEFT JOIN Product ON Store_Product.id_product = Product.id_product " +
+                         "LEFT JOIN Receipt ON Sale.check_number = Receipt.receipt_number " +
+                         $"WHERE DATE(Receipt.print_date) >= '{minPrintDateString}' " +
+                         $"AND DATE(Receipt.print_date) <= '{maxPrintDateString}'";
+            
+            if (productId != Constants.AllString)
+            {
+                sql += $" AND Product.id_product LIKE '%{productId}%'";
+            }
+            
+            List<string[]> result = DbUtils.FindAll(sql);
+            return result.Count != 0 ? result[0][0] : "0";
         }
     }
 }
