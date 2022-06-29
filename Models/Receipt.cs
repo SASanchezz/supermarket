0170276fb7f7474fb6d743a3cc2112e7a2c714a1
Alexander Hrachov <sgrachov42@gmail.com>
2022-06-29 18:58:38 +0300

    adeed discount showing for receipts

--- Models/Receipt.cs
+++ Models/Receipt.cs
@@ -17,6 +17,7 @@ namespace supermarket.Models
         public const int name_employee = 6;
         public const int surname_employee = 7;
         public const int patronymic_employee = 8;
+        public const int real_sum_total = 9;
 
         static int ERROR = -1;
         static int GOOD = 1;
@@ -41,8 +42,29 @@ namespace supermarket.Models
             }
 
             List<string[]> result = DbUtils.FindAll(sql);
+
+            List<string[]> newList = new();
+            result.ForEach(receipt =>
+            {
+                string[] newArray = new string[10];
+                Array.Copy(receipt, newArray, 9);
+                newArray[9] = Math.Round(getRealReceiptSum(receipt[receipt_number]), 2).ToString();
+                newList.Add(newArray);
+
+            });
             
-            return result.Count > 0 ? result : null;
+            return newList.Count > 0 ? newList : null;
+        }
+
+        public static string[] GetReceiptByReceiptNumber(string receiptNumber)
+        {
+            string sql = $"SELECT * " +
+                         $"FROM Receipt " +
+                         $"WHERE receipt_number = '{receiptNumber}'";
+
+            List<string[]> result = DbUtils.FindAll(sql);
+
+            return result.Count > 0 ? result[0] : null;
         }
 
         public static double GetAllReceiptsSum(string idCashier, DateTime minPrintDate, DateTime maxPrintDate)
@@ -65,7 +87,15 @@ namespace supermarket.Models
             string receiptId = IdUtils.ReceiptId();
             string vat = (double.Parse(sumTotal) * Constants.VatPercent).ToString().Replace(',', '.');
             sumTotal = sumTotal.Replace(',', '.');
-            cardNumber = cardNumber == "" ? "NULL" : $"'{cardNumber}'";
+            if (cardNumber == "")
+            {
+                cardNumber = "NULL";
+            } else
+            {
+                string[] customer = Customer.GetCustomerByCardNumber(cardNumber);
+                sumTotal = (double.Parse(sumTotal) * (100 - double.Parse(customer[Customer.percent])) / 100).ToString();
+                cardNumber = $"'{cardNumber}'";
+            }
 
             string sql = "INSERT INTO Receipt " +
                          "(receipt_number, id_employee, card_number, print_date, sum_total, vat) " +
@@ -95,6 +125,29 @@ namespace supermarket.Models
 
             }
             return response;
+        }
+
+        public static double getRealReceiptSum(string receiptNumber)
+        {
+            double finalSum = 0;
+
+            List<string[]> receiptSales = Sale.GetAllSalesByCheckNumber(receiptNumber);
+            receiptSales.ForEach(sale =>
+            {
+                finalSum += double.Parse(sale[Sale.selling_price]);
+            });
+
+            return Math.Round(finalSum, 2);
+        }
+
+        public static string GetReceiptCustomerDiscount(string receiptNumber)
+        {
+            string[] receipt = GetReceiptByReceiptNumber(receiptNumber);
+            if (receipt[card_number] == null || receipt[card_number] == "") return "0";
+
+            string[] customer = Customer.GetCustomerByCardNumber(receipt[card_number]);
+
+            return customer[Customer.percent];
         }
     }
 }
